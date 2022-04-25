16fc9b387e58a594216b961d3a00041fd1e0c372
MORETTE Valentin <morette.valentin@gmail.com>
2022-04-25 17:33:03 +0200

    change button download

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -4,7 +4,7 @@ import CVPDF from "../assets/CV_VM_DL.pdf";
 function Cv () {
     return (
         <>
-        <div className="dl"><a href={CVPDF} download="CV_VM_DL.pdf"><button className="buttondl">Download &emsp;<i class="fa-solid fa-file-arrow-down"></i></button></a></div>
+        <div className="dl"><a href={CVPDF} download="CV_VM_DL.pdf" className="linkdl">Download <i class="fa-solid fa-file-arrow-down"></i></a></div>
         <div id="cv">
             <img id="imgcv" src={CV} alt="CV"></img>
         </div>
