ce456ce1a4f5c1052bea657f5e12999228ef9bd5
MORETTE Valentin <morette.valentin@gmail.com>
2022-06-08 21:13:55 +0200

    change cv

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -1,15 +1,19 @@
-import CV from "../assets/CV_VM.png";
-import CVPDF from "../assets/CV_VM_DL.pdf";
+import CV from "../assets/CV_VMorette.png";
+import CVPDF from "../assets/CV_VMorette.pdf";
 
-function Cv () {
-    return (
-        <>
-        <div className="dl"><a href={CVPDF} download="CV_VM_DL.pdf" className="linkdl">Download <i class="fa-solid fa-file-arrow-down"></i></a></div>
-        <div id="cv">
-            <img id="imgcv" src={CV} alt="CV"></img>
-        </div>
-        </>
-    )
+function Cv() {
+  return (
+    <>
+      <div className="dl">
+        <a href={CVPDF} download="CV_VM_DL.pdf" className="linkdl">
+          Download <i class="fa-solid fa-file-arrow-down"></i>
+        </a>
+      </div>
+      <div id="cv">
+        <img id="imgcv" src={CV} alt="CV"></img>
+      </div>
+    </>
+  );
 }
 
 export default Cv;
