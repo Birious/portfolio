79408f1d354eac4b8bb4f8c8e669bab8e0b5bf00
MORETTE Valentin <morette.valentin@gmail.com>
2022-07-26 11:28:51 +0200

    add project sam

--- src/datas/dataProjects.jsx
+++ src/datas/dataProjects.jsx
@@ -1,44 +1,52 @@
-import poster from '../assets/images/img_projet/posteries.png';
-import trombi from '../assets/images/img_projet/trombi.png';
-import coockie from '../assets/images/img_projet/coockie_cliker.png';
-import wcw from '../assets/images/img_projet/wcw.png';
-import mario from '../assets/images/img_projet/mario.png';
+import poster from "../assets/images/img_projet/posteries.png";
+import trombi from "../assets/images/img_projet/trombi.png";
+import coockie from "../assets/images/img_projet/coockie_cliker.png";
+import wcw from "../assets/images/img_projet/wcw.png";
+import mario from "../assets/images/img_projet/mario.png";
+import sam from "../assets/images/img_projet/sam.png";
 
 const dataProjects = [
   {
-    title: 'Trombinoscope',
+    title: "Shop Around Me (Mobile Only)",
     description:
-      'Premier projet de groupe à la Wild Code School. Trombinoscope de notre promotion',
-    illus: trombi,
-    lien: 'https://Valentin-Morette.github.io/trombinoscope/',
+      "Troisième projet de groupe à la Wild Code School. Application Mobile pour trouver des produits proches de chez vous. Email : user , Password : password",
+    illus: sam,
+    lien: "https://theodep.github.io/Shop-Around-Me/",
   },
   {
-    title: 'Posteries',
+    title: "Posteries",
     description:
       "Second projet de groupe à la Wild Code School. Jeu faisant appel à l'API 'The Movie Database', qui consiste à deviner le plus rapidement possible le poster d'un film.",
     illus: poster,
-    lien: 'https://github.com/Valentin-Morette/project-2-posteries',
+    lien: "https://theodep.github.io/Posteries/#/",
   },
   {
-    title: 'Wild Code Wars',
+    title: "Trombinoscope",
+    description:
+      "Premier projet de groupe à la Wild Code School. Trombinoscope de notre promotion",
+    illus: trombi,
+    lien: "https://Valentin-Morette.github.io/trombinoscope/",
+  },
+  {
+    title: "Wild Code Wars",
     description:
       "Page utilisant l'API du jeu d'algorithme Code Wars, regroupe les statistiques de toute la promotion de la Wild Code School sur le jeu.",
     illus: wcw,
-    lien: 'https://Valentin-Morette.github.io/wild-code-wars/',
+    lien: "https://Valentin-Morette.github.io/wild-code-wars/",
   },
   {
-    title: 'Mario 64 Soluce',
+    title: "Mario 64 Soluce",
     description:
       "Site de solution du jeu Mario 64 sous format vidéo. Projet personnel pour m'entraîner avec une base de données.",
     illus: mario,
-    lien: 'https://github.com/Valentin-Morette/m64soluce',
+    lien: "https://github.com/Valentin-Morette/m64soluce",
   },
   {
-    title: 'Coockie-Cliker',
+    title: "Coockie-Cliker",
     description:
       "Jeu qui reprend le principe du jeu Cookie-Clicker, c'est un terrain de jeu pour m'exercer à React en dehors des exercices des cours.",
     illus: coockie,
-    lien: 'https://Valentin-Morette.github.io/coockie_cliker/',
+    lien: "https://Valentin-Morette.github.io/coockie_cliker/",
   },
 ];
 
